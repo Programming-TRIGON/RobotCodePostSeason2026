71db62ff1a22c95477977d8a1a7e8f6a87e0b72a
Yinon Dahan <ynnon.dahan@gmail.com>
2026-05-19 15:58:13 +0300

    Coded the Indexer Subsystem

--- src/main/java/frc/trigon/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/trigon/robot/subsystems/indexer/Indexer.java
@@ -1,4 +1,99 @@
 package frc.trigon.robot.subsystems.indexer;
 
-public class Indexer {
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
+import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXSignal;
+import frc.trigon.lib.hardware.phoenix6.talonfxs.TalonFXSSignal;
+import frc.trigon.robot.subsystems.MotorSubsystem;
+
+public class Indexer extends MotorSubsystem {
+    //private final ShootingCalculations shootingCalculations = ShootingCalculations.getInstance();
+    //change after nahum hahamud gomer
+    private final TalonFXMotor motor = IndexerConstants.MOTOR;
+    private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(IndexerConstants.FOC_ENABLED);
+    private final MotionMagicVelocityVoltage velocityRequest = new MotionMagicVelocityVoltage(0).withEnableFOC(IndexerConstants.FOC_ENABLED);
+    private double targetVelocityMetersPerSecond;
+
+    public Indexer(){
+        setName("Indexer");
+    }
+
+    @Override
+    public void updateLog(SysIdRoutineLog log) {
+        log.motor("SpindexerMotor")
+                .angularPosition(Units.Rotations.of(motor.getSignal(TalonFXSignal.POSITION)))
+                .angularVelocity(Units.RotationsPerSecond.of(getCurrentVelocityMetersPerSecond()))
+                .voltage(Units.Volts.of(motor.getSignal(TalonFXSignal.MOTOR_VOLTAGE)));
+    }
+
+    @Override
+    public void updateMechanism() {
+        IndexerConstants.MECHANISM.update(
+                getCurrentVelocityMetersPerSecond(),
+                motor.getSignal(TalonFXSignal.CLOSED_LOOP_REFERENCE)
+        );
+    }
+
+    @Override
+    public void sysIDDrive(double targetVoltage) {
+        motor.setControl(voltageRequest.withOutput(targetVoltage));
+    }
+
+    @Override
+    public SysIdRoutine.Config getSysIDConfig() {
+        return IndexerConstants.SYSID_CONFIG;
+    }
+
+    @Override
+    public void updatePeriodically() {
+        motor.update();
+    }
+
+    @Override
+    public void stop() {
+        motor.stopMotor();
+        targetVelocityMetersPerSecond = 0;
+    }
+
+    public boolean atTargetState(IndexerConstants.IndexerState targetState) {
+        return atVelocity(targetState.targetVelocityMetersPerSecond);
+    }
+
+    public boolean atVelocity(double velocityMetersPerSecond) {
+        return Math.abs(getCurrentVelocityMetersPerSecond() - velocityMetersPerSecond)
+                <= IndexerConstants.VELOCITY_TOLERANCE_METERS_PER_SECOND;
+    }
+
+    public Pose3d calculateComponentPose() {
+        final Transform3d yawTransform = new Transform3d(
+                new Translation3d(0, 0, 0),
+                new Rotation3d(0, 0, Rotation2d.fromRotations(motor.getSignal(TalonFXSignal.POSITION)).getRadians() * IndexerConstants.SIMULATION_SLIPPAGE_COMPENSATION_MULTIPLIER)
+        );
+        return IndexerConstants.VISUALIZATION_ORIGIN_POSE.transformBy(yawTransform);
+    }
+
+    void loadToShooter() {
+        //final double targetShooterVelocityFromShootingCalculations = shootingCalculations.getTargetShootingState().targetShootingVelocityMetersPerSecond();
+        //final double targetLoadingVelocity = targetShooterVelocityFromShootingCalculations * IndexerConstants.LOADING_SPEED_RELATIVE_TO_SHOOTING_COEFFICIENT;
+        //change after nahum hahamud gomer
+        setTargetVelocity(12.1205319);
+    }
+
+    void setTargetState(IndexerConstants.IndexerState targetState) {
+        setTargetVelocity(targetState.targetVelocityMetersPerSecond);
+    }
+
+    void setTargetVelocity(double targetVelocityMetersPerSecond) {
+        this.targetVelocityMetersPerSecond = targetVelocityMetersPerSecond;
+        motor.setControl(velocityRequest.withVelocity(targetVelocityMetersPerSecond));
+    }
+
+    private double getCurrentVelocityMetersPerSecond() {
+        return motor.getSignal(TalonFXSignal.VELOCITY);
+    }
 }
--- src/main/java/frc/trigon/robot/subsystems/indexer/IndexerCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/indexer/IndexerCommands.java
@@ -1,4 +1,41 @@
 package frc.trigon.robot.subsystems.indexer;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.trigon.lib.commands.ExecuteEndCommand;
+import frc.trigon.lib.commands.NetworkTablesCommand;
+import frc.trigon.robot.RobotContainer;
+
 public class IndexerCommands {
+    public static Command getDebuggingCommand() {
+        return new NetworkTablesCommand(
+                IndexerCommands::getSetTargetVelocityCommand,
+                false,
+                "Debugging/SpindexerTargetVelocityMetersPerSecond"
+        );
+    }
+
+    public static Command getSetTargetStateCommand(IndexerConstants.IndexerState targetState) {
+        return new StartEndCommand(
+                () -> RobotContainer.INDEXER.setTargetState(targetState),
+                RobotContainer.INDEXER::stop,
+                RobotContainer.INDEXER
+        );
+    }
+
+    public static Command getSetTargetVelocityCommand(double targetVelocityMetersPerSecond) {
+        return new StartEndCommand(
+                () -> RobotContainer.INDEXER.setTargetVelocity(targetVelocityMetersPerSecond),
+                RobotContainer.INDEXER::stop,
+                RobotContainer.INDEXER
+        );
+    }
+
+    public static Command getLoadToShooterCommand() {
+        return new ExecuteEndCommand(
+                RobotContainer.INDEXER::loadToShooter,
+                RobotContainer.INDEXER::stop,
+                RobotContainer.INDEXER
+        );
+    }
 }
