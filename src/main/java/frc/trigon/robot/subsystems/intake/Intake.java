51d2173defb7eaac364e2ae8f5135c1f0af3ef5b
noamgreen12 <noamarielgreen@gmail.com>
2026-05-21 01:21:13 +0300

    Added rest of the SysID things.

--- src/main/java/frc/trigon/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/trigon/robot/subsystems/intake/Intake.java
@@ -3,6 +3,9 @@ package frc.trigon.robot.subsystems.intake;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXSignal;
 import frc.trigon.robot.subsystems.MotorSubsystem;
@@ -21,15 +24,39 @@ public class Intake extends MotorSubsystem {
     }
 
     @Override
+    public void updateLog(SysIdRoutineLog log) {
+        log.motor("IntakeAngleMotor")
+                .angularPosition(Units.Rotations.of(getCurrentIntakeArmAngle().getRotations()))
+                .angularVelocity(Units.RotationsPerSecond.of(masterAngleMotor.getSignal(TalonFXSignal.VELOCITY)))
+                .voltage(Units.Volts.of(masterAngleMotor.getSignal(TalonFXSignal.MOTOR_VOLTAGE)));
+    }
+
+    @Override
     public void updateMechanism() {
         IntakeConstants.INTAKE_ANGLE_MECHANISM.update(
-                getCurrentIntakeArmAngle()
+                getCurrentIntakeArmAngle(),
+                Rotation2d.fromRotations(masterAngleMotor.getSignal(TalonFXSignal.CLOSED_LOOP_REFERENCE))
         );
         IntakeConstants.INTAKE_MOTOR_MECHANISM.update(
                 getCurrentIntakeVoltage()
         );
 
         Logger.recordOutput("Poses/Components/IntakePose", calculateVisualizationPose());
+    }
+
+    @Override
+    public void sysIDDrive(double targetVoltage) {
+        masterAngleMotor.setControl(voltageRequest.withOutput(targetVoltage));
+    }
+
+    @Override
+    public SysIdRoutine.Config getSysIDConfig() {
+        return IntakeConstants.SYSID_CONFIG;
+    }
+
+    @Override
+    public void setBrake(boolean brake) {
+        masterAngleMotor.setBrake(brake);
     }
 
     @Override
@@ -44,7 +71,6 @@ public class Intake extends MotorSubsystem {
         masterAngleMotor.stopMotor();
         intakeMotor.stopMotor();
         IntakeConstants.INTAKE_MOTOR_MECHANISM.setTargetVelocity(0);
-        IntakeConstants.INTAKE_ANGLE_MECHANISM.setTargetAngle(Rotation2d.fromDegrees(90));
     }
 
     void setTargetState(IntakeConstants.IntakeState targetState) {
