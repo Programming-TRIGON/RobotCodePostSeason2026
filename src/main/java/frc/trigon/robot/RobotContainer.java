f40a7011abc406503bc653f7f1f0a47f7899de8c
Yinon Dahan <ynnon.dahan@gmail.com>
2026-05-26 22:32:12 +0300

    Loader (#7)

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -20,6 +20,9 @@ import frc.trigon.robot.misc.objectdetection.ObjectPoseEstimator;
 import frc.trigon.robot.misc.simulatedfield.SimulatedGamePieceConstants;
 import frc.trigon.robot.poseestimation.robotposeestimator.RobotPoseEstimator;
 import frc.trigon.robot.subsystems.MotorSubsystem;
+import frc.trigon.robot.subsystems.loader.Loader;
+import frc.trigon.robot.subsystems.loader.LoaderCommands;
+import frc.trigon.robot.subsystems.loader.LoaderConstants;
 import frc.trigon.robot.subsystems.hood.Hood;
 import frc.trigon.robot.subsystems.hood.HoodCommands;
 import frc.trigon.robot.subsystems.swerve.Swerve;
@@ -34,6 +37,7 @@ public class RobotContainer {
     );
     public static final Swerve SWERVE = new Swerve();
     public static final Hood HOOD = new Hood();
+    public static final Loader LOADER = new Loader();
     private LoggedDashboardChooser<Command> autoChooser;
 
     public RobotContainer() {
@@ -57,6 +61,7 @@ public class RobotContainer {
     private void bindDefaultCommands() {
         SWERVE.setDefaultCommand(GeneralCommands.getFieldRelativeDriveCommand());
         HOOD.setDefaultCommand(HoodCommands.getRestCommand());
+        LOADER.setDefaultCommand(LoaderCommands.getSetTargetStateCommand(LoaderConstants.LoaderState.REST));
     }
 
     private void bindControllerCommands() {
---
+++ src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
@@ -0,0 +1,109 @@
+package frc.trigon.robot.subsystems.loader;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
+import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXSignal;
+import frc.trigon.lib.hardware.simulation.SimpleMotorSimulation;
+import frc.trigon.lib.utilities.mechanisms.SpeedMechanism2d;
+
+public class LoaderConstants {
+    private static final int
+            MASTER_MOTOR_ID = 13,
+            FOLLOWER_MOTOR_ID = 14;
+    private static final String
+            MASTER_MOTOR_NAME = "LoaderMasterMotor",
+            FOLLOWER_MOTOR_NAME = "LoaderFollowerMotor";
+    static final TalonFXMotor
+            MASTER_MOTOR = new TalonFXMotor(MASTER_MOTOR_ID, MASTER_MOTOR_NAME),
+            FOLLOWER_MOTOR = new TalonFXMotor(FOLLOWER_MOTOR_ID, FOLLOWER_MOTOR_NAME);
+
+    static final boolean FOC_ENABLED = true;
+    private static final double GEAR_RATIO = 4;
+    private static final MotorAlignmentValue FOLLOWER_ALIGNMENT_TO_MASTER = MotorAlignmentValue.Aligned;
+
+    private static final int MOTOR_AMOUNT = 2;
+    private static final DCMotor GEARBOX = DCMotor.getKrakenX44(MOTOR_AMOUNT);
+    private static final double MOMENT_OF_INERTIA = 0.003;
+    static final SimpleMotorSimulation SIMULATION = new SimpleMotorSimulation(
+            GEARBOX,
+            GEAR_RATIO,
+            MOMENT_OF_INERTIA
+    );
+
+    private static final double MAXIMUM_DISPLAYABLE_VOLTAGE = 12;
+    private static final String LOADER_MECHANISM_NAME = "LoaderMechanism";
+    static final SpeedMechanism2d LOADER_MECHANISM = new SpeedMechanism2d(
+            LOADER_MECHANISM_NAME,
+            MAXIMUM_DISPLAYABLE_VOLTAGE
+    );
+
+    static {
+        configureLoaderMasterMotor();
+        configureLoaderFollowerMotor();
+    }
+
+    private static void configureLoaderMasterMotor() {
+        final TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.Audio.BeepOnBoot = false;
+        config.Audio.BeepOnConfig = false;
+
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        config.Feedback.SensorToMechanismRatio = GEAR_RATIO;
+
+        config.CurrentLimits.StatorCurrentLimit = 50;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        MASTER_MOTOR.applyConfiguration(config);
+        MASTER_MOTOR.setPhysicsSimulation(SIMULATION);
+
+        MASTER_MOTOR.registerSignal(TalonFXSignal.STATOR_CURRENT, 100);
+        MASTER_MOTOR.registerSignal(TalonFXSignal.MOTOR_VOLTAGE, 100);
+    }
+
+    private static void configureLoaderFollowerMotor() {
+        final TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.Audio.BeepOnBoot = false;
+        config.Audio.BeepOnConfig = false;
+
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        config.Feedback.SensorToMechanismRatio = GEAR_RATIO;
+
+        config.CurrentLimits.StatorCurrentLimit = 50;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        FOLLOWER_MOTOR.applyConfiguration(config);
+        FOLLOWER_MOTOR.setPhysicsSimulation(SIMULATION);
+
+        final Follower followRequest = new Follower(MASTER_MOTOR.getID(), FOLLOWER_ALIGNMENT_TO_MASTER);
+        FOLLOWER_MOTOR.setControl(followRequest);
+
+        FOLLOWER_MOTOR.registerSignal(TalonFXSignal.STATOR_CURRENT, 100);
+        FOLLOWER_MOTOR.registerSignal(TalonFXSignal.MOTOR_VOLTAGE, 100);
+    }
+
+    public enum LoaderState {
+        LOAD_FOR_DELIVERY(6),
+        LOAD_FOR_SHOOTING(4),
+        LOAD_FOR_EJECTION(4),
+        REST(0);
+
+        public final double targetVoltage;
+
+        LoaderState(double targetVoltage) {
+            this.targetVoltage = targetVoltage;
+        }
+    }
+}
