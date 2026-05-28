740bc50f00da9d08463f5e97a83795f2021f7c86
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2026-05-29 02:45:54 +0300

    implemented most of camera constants

--- src/main/java/frc/trigon/robot/constants/CameraConstants.java
+++ src/main/java/frc/trigon/robot/constants/CameraConstants.java
@@ -1,5 +1,37 @@
 package frc.trigon.robot.constants;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCamera;
+import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCameraConstants;
+import frc.trigon.robot.poseestimation.robotposeestimator.StandardDeviations;
+
 public class CameraConstants {
-    //TODO: implement tag cameras
+    private static final StandardDeviations APRIL_TAG_CAMERA_STANDARD_DEVIATIONS = new StandardDeviations(
+            0.016,
+            0.01
+    );
+    private static final Transform3d
+            ROBOT_TO_RIGHT_APRIL_TAG_CAMERA = new Transform3d(
+            new Translation3d(),
+            new Rotation3d()
+    ), //TODO: get actual transforms
+            ROBOT_TO_LEFT_APRIL_TAG_CAMERA = new Transform3d(
+                    new Translation3d(),
+                    new Rotation3d()
+            ); //TODO: get actual transforms
+    public static final AprilTagCamera
+            RIGHT_APRIL_TAG_CAMERA = new AprilTagCamera(
+            AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
+            "RightAprilTagCamera",
+            ROBOT_TO_RIGHT_APRIL_TAG_CAMERA,
+            APRIL_TAG_CAMERA_STANDARD_DEVIATIONS
+    ),
+            LEFT_APRIL_TAG_CAMERA = new AprilTagCamera(
+                    AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
+                    "LeftAprilTagCamera",
+                    ROBOT_TO_LEFT_APRIL_TAG_CAMERA,
+                    APRIL_TAG_CAMERA_STANDARD_DEVIATIONS
+            );
 }
