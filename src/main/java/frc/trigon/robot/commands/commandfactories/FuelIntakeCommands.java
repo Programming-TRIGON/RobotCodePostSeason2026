c87c0f308f83509e0219ad685b8dfb5acf520e16
NoamAlternative <githubalternative16171@gmail.com>
2026-05-31 21:19:46 +0300

    deleted imports

--- src/main/java/frc/trigon/robot/commands/commandfactories/FuelIntakeCommands.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/FuelIntakeCommands.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj2.command.*;
 import frc.trigon.robot.constants.OperatorConstants;
 import frc.trigon.robot.subsystems.indexer.IndexerCommands;
 import frc.trigon.robot.subsystems.indexer.IndexerConstants;
-import frc.trigon.robot.subsystems.intake.IntakeConstants;
-import frc.trigon.robot.subsystems.intake.IntakeCommands;
 import frc.trigon.robot.subsystems.loader.LoaderCommands;
 import frc.trigon.robot.subsystems.loader.LoaderConstants;
 import edu.wpi.first.wpilibj2.command.Command;
