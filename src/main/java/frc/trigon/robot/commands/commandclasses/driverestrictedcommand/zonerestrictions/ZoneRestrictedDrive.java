af763fbdde0432abc308ddabc2a0b1a71a2b06d0
noamgreen12 <noamarielgreen@gmail.com>
2026-06-10 21:14:49 +0300

    Fixed it now.

--- src/main/java/frc/trigon/robot/commands/commandclasses/driverestrictedcommand/zonerestrictions/ZoneRestrictedDrive.java
+++ src/main/java/frc/trigon/robot/commands/commandclasses/driverestrictedcommand/zonerestrictions/ZoneRestrictedDrive.java
@@ -53,12 +53,7 @@ public class ZoneRestrictedDrive implements DriveRestriction {
 
     private BoundingBox getRobotBoundingBox() {
         final Pose2d robotPose = RobotContainer.ROBOT_POSE_ESTIMATOR.getEstimatedRobotPose();
-        final BoundingBox robotBoundingBox = ZoneRestrictedDriveConstants.ROBOT_RELATIVE_BOUNDING_BOX;
 
-        return new BoundingBox(
-                new Pose2d(robotBoundingBox.getCenter().getTranslation(), robotPose.getRotation()),
-                robotBoundingBox.getXWidth(),
-                robotBoundingBox.getYWidth()
-                );
+        return new BoundingBox(robotPose, ZoneRestrictedDriveConstants.ROBOT_X_WIDTH_METERS, ZoneRestrictedDriveConstants.ROBOT_Y_WIDTH_METERS);
     }
 }
