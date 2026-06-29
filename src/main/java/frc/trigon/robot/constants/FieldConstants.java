7ff203a84ffb8a52b4dd1e97082d8efaa3309806
TRIGON <Trigon5990@gmail.com>
2026-06-29 09:17:17 +0300

    removed redundent stuff
    Co-Authored-By: Nummun14 <142012009+Nummun14@users.noreply.github.com>

--- src/main/java/frc/trigon/robot/constants/FieldConstants.java
+++ src/main/java/frc/trigon/robot/constants/FieldConstants.java
@@ -21,13 +21,6 @@ public class FieldConstants {
     public static final double
             FIELD_WIDTH_METERS = FlippingUtil.fieldSizeY,
             FIELD_LENGTH_METERS = FlippingUtil.fieldSizeX;
-    public static final double
-            ALLIANCE_ZONE_LENGTH = 4.5,
-            DELIVERY_ZONE_START_BLUE_X = ALLIANCE_ZONE_LENGTH + 0.93,
-            TRENCH_ZONE_MINIMUM_X = 4.4,
-            TRENCH_ZONE_MAXIMUM_X = 4.9,
-            LEFT_TRENCH_MIN_Y = 6.9,
-            RIGHT_TRENCH_MAX_Y = FIELD_WIDTH_METERS - LEFT_TRENCH_MIN_Y;
     public static final BoundingBox FIELD_BOUNDING_BOX = new BoundingBox(
             new Translation2d(0, 0),
             new Translation2d(FIELD_LENGTH_METERS, FIELD_WIDTH_METERS)
@@ -43,18 +36,7 @@ public class FieldConstants {
     public static final FlippableTranslation2d RIGHT_OF_TOWER_POSITION = new FlippableTranslation2d(0.949, 2.531, true);
     public static final FlippableTranslation2d LEFT_OF_TOWER_POSITION = new FlippableTranslation2d(0.716, 4.827, true);
 
-    public static final double
-            DEPOT_X = 0.45,
-            DEPOT_Y = 7.0,
-            INTAKE_X = 7.5,
-            INTAKE_Y = FIELD_WIDTH_METERS / 2,
-            FIRST_INTAKE_Y = 6.8,
-            IDEAL_SHOOTING_X = 4.3,
-            IDEAL_SHOOTING_Y = 7.48,
-            TRENCH_ALLIANCE_ENTRY_AUTONOMOUS_X = 3.4,
-            TRENCH_NEUTRAL_ENTRY_AUTONOMOUS_X = 5.58,
-            TRENCH_ENTRY_Y = 7.48,
-            ALLIANCE_ZONE_LENGTH_METERS = 4.5;
+    public static final double ALLIANCE_ZONE_LENGTH_METERS = 4.5;
     private static final double
             BLUE_RELATIVE_DELIVERY_POSITION_X = 3.0,
             DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS = 2.2;
@@ -66,19 +48,6 @@ public class FieldConstants {
             HUB_POSITION = new FlippableTranslation2d(TAG_ID_TO_POSE.get(26).getX() + (Units.inchesToMeters(47) / 2), FIELD_WIDTH_METERS / 2, true),
             RIGHT_DELIVERY_POSITION = new FlippableTranslation2d(BLUE_RELATIVE_DELIVERY_POSITION_X, (FIELD_WIDTH_METERS / 2) - DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS, true),
             LEFT_DELIVERY_POSITION = new FlippableTranslation2d(BLUE_RELATIVE_DELIVERY_POSITION_X, (FIELD_WIDTH_METERS / 2) + DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS, true);
-
-    public static final FlippablePose2d
-            DEPOT_POSITION = new FlippablePose2d(DEPOT_X, DEPOT_Y, Rotation2d.fromDegrees(-90), true),
-            LEFT_INTAKE_POSITION = new FlippablePose2d(INTAKE_X, INTAKE_Y, Rotation2d.fromDegrees(-80), true),
-            RIGHT_INTAKE_POSITION = mirror(LEFT_INTAKE_POSITION),
-            LEFT_FIRST_INTAKE_POSITION = new FlippablePose2d(FIELD_LENGTH_METERS / 2.0, FIRST_INTAKE_Y, Rotation2d.fromDegrees(-100), true),
-            RIGHT_FIRST_INTAKE_POSITION = mirror(LEFT_FIRST_INTAKE_POSITION),
-            LEFT_IDEAL_SHOOTING_POSITION = new FlippablePose2d(IDEAL_SHOOTING_X, IDEAL_SHOOTING_Y, Rotation2d.kZero, true),
-            RIGHT_IDEAL_SHOOTING_POSITION = mirror(LEFT_IDEAL_SHOOTING_POSITION),
-            LEFT_TRENCH_ENTRY_POSITION_FROM_ALLIANCE_ZONE = new FlippablePose2d(TRENCH_ALLIANCE_ENTRY_AUTONOMOUS_X, TRENCH_ENTRY_Y, Rotation2d.kZero, true),
-            RIGHT_TRENCH_ENTRY_POSITION_FROM_ALLIANCE_ZONE = mirror(LEFT_TRENCH_ENTRY_POSITION_FROM_ALLIANCE_ZONE),
-            LEFT_TRENCH_ENTRY_POSITION_FROM_NEUTRAL_ZONE = new FlippablePose2d(TRENCH_NEUTRAL_ENTRY_AUTONOMOUS_X, TRENCH_ENTRY_Y, Rotation2d.kZero, true),
-            RIGHT_TRENCH_ENTRY_POSITION_FROM_NEUTRAL_ZONE = mirror(LEFT_TRENCH_ENTRY_POSITION_FROM_NEUTRAL_ZONE);
 
     /**
      * The trench bounding box coordinates in meters.
@@ -153,28 +122,6 @@ public class FieldConstants {
                 Rotation2d.fromDegrees(-basePose.getRotation().getDegrees()),
                 true
         );
-    }
-
-    public static boolean isRobotInTrenchYRange() {
-        return isPoseInTrenchYRange(RobotContainer.ROBOT_POSE_ESTIMATOR.getEstimatedRobotPose().getTranslation());
-    }
-
-    public static boolean isPoseInTrenchYRange(Translation2d pose) {
-        if (pose == null)
-            return false;
-        return pose.getY() > FieldConstants.LEFT_TRENCH_MIN_Y || pose.getY() < FieldConstants.RIGHT_TRENCH_MAX_Y;
-    }
-
-    public static boolean isRobotInDeliveryZone() {
-        return isPoseInDeliveryZone(RobotContainer.ROBOT_POSE_ESTIMATOR.getEstimatedRobotPose().getTranslation());
-    }
-
-    public static boolean isPoseInDeliveryZone(Translation2d pose) {
-        if (pose == null)
-            return false;
-        if (Flippable.isRedAlliance())
-            return pose.getX() < FieldConstants.FIELD_LENGTH_METERS - FieldConstants.DELIVERY_ZONE_START_BLUE_X;
-        return pose.getX() > FieldConstants.DELIVERY_ZONE_START_BLUE_X;
     }
 
     public static boolean isRobotInAllianceZone() {
