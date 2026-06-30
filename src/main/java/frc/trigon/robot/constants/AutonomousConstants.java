41ff0a60b28053b602497ea09375bbeded929073
TRIGON <Trigon5990@gmail.com>
2026-06-30 16:01:41 +0300

    qual 20
    Co-Authored-By: Nummun14 <142012009+Nummun14@users.noreply.github.com>

--- src/main/java/frc/trigon/robot/constants/AutonomousConstants.java
+++ src/main/java/frc/trigon/robot/constants/AutonomousConstants.java
@@ -61,7 +61,7 @@ public class AutonomousConstants {
     private static void configureAutoBuilder() {
         AutoBuilder.configure(
                 RobotContainer.ROBOT_POSE_ESTIMATOR::getEstimatedRobotPose,
-                RobotContainer.ROBOT_POSE_ESTIMATOR::resetPose,
+                (a) -> {},
                 RobotContainer.SWERVE::getSelfRelativeChassisSpeeds,
                 RobotContainer.SWERVE::drivePathPlanner,
                 AUTO_PATH_FOLLOWING_CONTROLLER,
