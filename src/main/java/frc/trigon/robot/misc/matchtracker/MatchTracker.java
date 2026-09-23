7fbd586fb8c51cedf92b13447d0c58a900c32b5b
noamgreen12 <noamarielgreen@gmail.com>
2026-09-23 15:16:49 +0300

    made match tracker

---
+++ src/main/java/frc/trigon/robot/misc/matchtracker/MatchTracker.java
@@ -0,0 +1,127 @@
+package frc.trigon.robot.misc.matchtracker;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.trigon.lib.utilities.flippable.Flippable;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
+
+public class MatchTracker {
+    private static boolean isRedAlliance = Flippable.isRedAlliance();
+    private static final LoggedNetworkBoolean hubActiveOverride = new LoggedNetworkBoolean("MatchTracker/HubActiveOverride", false);
+
+    @AutoLogOutput(key = "MatchTracker/IsHubActive")
+    public static boolean isHubActive() {
+        double currentMatchTimeSeconds = getCurrentMatchTimeSeconds();
+        char autoWinner = getAutoWinner();
+
+        if (hubActiveOverride.get()) {
+            return true;
+        }
+
+        if (DriverStation.isAutonomousEnabled())
+            return true;
+
+        if (autoWinner != 'R' && autoWinner != 'B') {
+            return false;
+        }
+
+        if (currentMatchTimeSeconds < MatchTrackerConstants.END_GAME_SHIFT_START_TIME_SECONDS)
+            return true;
+
+        if (autoWinner == 'R') {
+            if (currentMatchTimeSeconds < MatchTrackerConstants.FOURTH_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.THIRD_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return !isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.SECOND_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.FIRST_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return !isRedAlliance;
+            }
+        }
+
+        if (autoWinner == 'B') {
+            if (currentMatchTimeSeconds < MatchTrackerConstants.FOURTH_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return !isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.THIRD_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.SECOND_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return !isRedAlliance;
+            }
+
+            if (currentMatchTimeSeconds < MatchTrackerConstants.FIRST_SHIFT_START_TIME_SECONDS + MatchTrackerConstants.HUB_ACTIVATION_EARLY_SECONDS) {
+                return isRedAlliance;
+            }
+        }
+
+        return currentMatchTimeSeconds < MatchTrackerConstants.TRANSITION_SHIFT_START_TIME_SECONDS;
+    }
+
+    @AutoLogOutput(key = "MatchTracker/TimeUntilNextShift")
+    private static double getTimeUntilNextShift() {
+        double currentMatchTimeSeconds = getCurrentMatchTimeSeconds();
+
+        if (DriverStation.isAutonomousEnabled()) {
+            return currentMatchTimeSeconds;
+        }
+
+        if (!DriverStation.isTeleopEnabled()) {
+            return 0;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.TRANSITION_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.TRANSITION_SHIFT_START_TIME_SECONDS;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.FIRST_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.FIRST_SHIFT_START_TIME_SECONDS;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.SECOND_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.SECOND_SHIFT_START_TIME_SECONDS;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.THIRD_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.THIRD_SHIFT_START_TIME_SECONDS;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.FOURTH_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.FOURTH_SHIFT_START_TIME_SECONDS;
+        }
+
+        if (currentMatchTimeSeconds > MatchTrackerConstants.END_GAME_SHIFT_START_TIME_SECONDS) {
+            return currentMatchTimeSeconds - MatchTrackerConstants.END_GAME_SHIFT_START_TIME_SECONDS;
+        }
+
+        return currentMatchTimeSeconds;
+    }
+
+    @AutoLogOutput(key = "MatchTracker/SecondsLeftInMatch")
+    private static double getCurrentMatchTimeSeconds() {
+        return DriverStation.getMatchTime();
+    }
+
+    private static char getAutoWinner() {
+        String gameData = DriverStation.getGameSpecificMessage();
+
+        if (!gameData.isEmpty()) {
+            char autoWinner = gameData.charAt(0);
+
+            if (autoWinner == 'R' || autoWinner == 'B') {
+                return autoWinner;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ src/main/java/frc/trigon/robot/misc/matchtracker/MatchTrackerConstants.java
@@ -0,0 +1,12 @@
+package frc.trigon.robot.misc.matchtracker;
+
+public class MatchTrackerConstants {
+    static final double
+            TRANSITION_SHIFT_START_TIME_SECONDS = 140,
+            FIRST_SHIFT_START_TIME_SECONDS = 130,
+            SECOND_SHIFT_START_TIME_SECONDS = 105,
+            THIRD_SHIFT_START_TIME_SECONDS = 80,
+            FOURTH_SHIFT_START_TIME_SECONDS = 55,
+            END_GAME_SHIFT_START_TIME_SECONDS = 30,
+            HUB_ACTIVATION_EARLY_SECONDS = 2;
+}
